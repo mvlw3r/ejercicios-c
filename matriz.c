40c602cc5ecdc6ebd79d6887398cef0e30f69f1e
esteban <estebansanchezsoto916@gmail.com>
2022-03-14 17:23:17 -0600

    matriz

--- matriz.c
+++ matriz.c
@@ -1,37 +1,45 @@
+/*
+
+Hecho por: Esteban
+Contacto:estebansanchezsoto916@gmail.com
+Sun 06 Mar 2022 05:19:11 PM CST
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#define  Maximo 100
 
 int main(){
 
-      int m[100]; 
+      int v[Maximo];
       int i;
       int j;
       int temp;
       srand(time(NULL));
 
 	for(i=0;i<100;i++){
-	m[i]=1+rand()%(500-1+1);
-	printf("%d\n",m[i]);
+	v[i]=1+rand()%(1000-1+1);
+	printf("%d\n",v[i]);
 }
 
 	for(i=0;i<=99;i++)
 	{
 		for(j=0;j<99;j++)
 		{
-			if(m[j]>m[j + 1])
+			if(v[j]>v[j + 1])
 			{
-				temp = m[j];
-				m[j] = m[j + 1];
-				m[j + 1] = temp;
+				temp = v[j];
+				v[j] = v[j + 1];
+				v[j + 1] = temp;
 			}
 		}
 	}
 
-for(i=0; i <= 99;i++)
-{
-printf("%d\n",m[i]);
-}
+   for(i=0; i <= 99;i++)
+   {
+     printf("%d\n",v[i]);
+   }
+
 return 0;
 }
