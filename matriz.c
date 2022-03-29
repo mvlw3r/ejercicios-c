2c44f2387cf8baf7464efee1142f5bca04c290ca
esteban <estebansanchezsoto916@gmail.com>
2022-03-28 19:38:58 -0600

    matriz

--- matriz.c
+++ matriz.c
@@ -4,7 +4,11 @@ Hecho por: Esteban
 Contacto:estebansanchezsoto916@gmail.com
 Sun 06 Mar 2022 05:19:11 PM CST
 
+Este programa esta hecho para crear valores aleatorios hasta el 1000
+seguidamente ordenarlos de menor a mayor.
+
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -16,6 +20,7 @@ int main(){
       int i;
       int j;
       int temp;
+
       srand(time(NULL));
 
 	for(i=0;i<100;i++){
